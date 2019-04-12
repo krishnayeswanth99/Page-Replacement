b38cc5c1290436853a5a8f3a162c2c0ef19cf6aa
Sasi Kiran <31450654+eonr@users.noreply.github.com>
2019-04-12 20:08:28 +0530

    Add time outputs

--- LRU_approx.cpp
+++ LRU_approx.cpp
@@ -1,15 +1,61 @@
 #include <iostream>
-#include<deque>
-#include<set>
-#include<algorithm>
-#include <utility>
+#include<vector>
 //0 4 1 4 2 4 3 4 2 4 0 4 1 4 2 4 3 4
 using namespace std;
+
+class Pair{
+  public:
+  int first;
+  bool second;
+  
+  Pair(){
+
+  }
+  Pair(int a, int b){
+    first = a;
+    second = b;
+  }
+};
+
+class Set{
+  public:
+  vector<int> vect;
+  vector<int>::iterator it;
+
+  int size(){
+    return vect.size();
+  }
+  void insert(int page){
+    vect.push_back(page);
+  }
+
+  bool find(int page){
+    bool found = false;
+    for(it = vect.begin();it != vect.end();it++){
+      if (*it==page){
+        found = true;
+        break;
+      }
+    }
+    return found;
+  }
+
+  void erase(int page){
+    for(it = vect.begin();it != vect.end();it++){
+      if (*it==page){
+        vect.erase(it);
+        break;
+      }
+    }
+  }
+};
+
+Set presentPages;
 int timer=0;
-set<int> presentPages;
 int one_counter = 0;
-void refer(int page,int max_size, pair<int, bool> pageTable[]){
-  if(presentPages.find(page) != presentPages.end()){
+int faults=0;
+void refer(int page,int max_size, Pair pageTable[]){
+  if(presentPages.find(page)){
     // Already exists
     cout<<"Page "<<page<<" already exists in the page table"<<endl;
     int i;
@@ -23,21 +69,23 @@ void refer(int page,int max_size, pair<int, bool> pageTable[]){
   }
   else if(presentPages.size() < max_size){
     //Doesn't exist - but has space
+    faults++;
     cout<<"Page fault: page "<<page<<" doesn't exist in the table but the table is not full"<<endl;
     presentPages.insert(page);
-    pageTable[timer] = make_pair(page, 0);
+    pageTable[timer] = Pair(page, 0);
     cout<<"Page "<<page<<" has been added to the page table"<<endl;
     timer = (timer+1)%max_size;
   }
   else{
     //Doesn't exist and all slots are occupied
+    faults++;
     cout<<"Page fault: page "<<page<<" doesn't exist in the table and the table is full"<<endl;
     //Finding next page with bool = 0
     for(int i = 0;i<max_size;i++){
       if(pageTable[timer].second==0){
         cout<<"Page "<<pageTable[timer].first<<" has been replaced by "<<page<<endl;
         presentPages.erase(pageTable[timer].first); //Removing old page from set
-        pageTable[timer] = make_pair(page,0);        //Adding new page to table
+        pageTable[timer] = Pair(page,0);        //Adding new page to table
         presentPages.insert(page);                //Adding new page to set
         break;
       }
@@ -57,7 +105,7 @@ int main() {
   cout<<"Enter no. of page references"<<endl;
   cin>>n;
 
-  pair<int, bool> pageTable[max_size];
+  Pair pageTable[max_size];
   int pages[n];
   cout<<"Enter "<<n<<" space separated page references"<<endl;
   for(int i=0;i<n;i++){
@@ -66,8 +114,15 @@ int main() {
     pages[i] = x;
   }
 
+  clock_t begin = clock();
   //Refer page by page
   for(int i=0;i<n;i++){
     refer(pages[i], max_size, pageTable);
   }
+  clock_t end = clock();
+
+  cout<<"\n\n\nTotal no. of page references: "<<n<<endl;
+  cout<<"Total no. of page faults: "<<faults<<endl;
+  double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+  cout<<"Time taken: "<<elapsed_secs<<endl;
 }
